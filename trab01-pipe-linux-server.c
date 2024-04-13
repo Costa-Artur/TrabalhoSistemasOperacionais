be49dad35925a15e421963997f7968f7590bc063
Artur Costa <souza.artur@edu.univali.br>
2024-04-13 12:44:06 -0300

    Fix linux pipe server code

--- trab01-pipe-linux-server.c
+++ trab01-pipe-linux-server.c
@@ -13,9 +13,10 @@ int main()
 {
     int sockfd, newsockfd, len;
     struct sockaddr_un local, remote;
-    char buffer[1024];
 
     int quantidade, peso, qtd_total=0, peso_total=0;
+    int items_received = 0;
+    struct timeval last_print_time, current_time;
 
     //Create Socket
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -38,7 +39,8 @@ int main()
         return 1;
     }
     
-    struct timeval last_print_time, current_time;
+    // Initialize last print time
+    gettimeofday(&last_print_time, NULL);
 
     while(1) {
 
@@ -46,14 +48,15 @@ int main()
         gettimeofday(&current_time, NULL);
 
         // Calculate elapsed time since last print
-        long elapsed_time = (current_time.tv_sec - last_print_time.tv_sec) * 1000000 + (current_time.tv_usec - last_print_time.tv_usec);
+        long elapsed_seconds = (current_time.tv_sec - last_print_time.tv_sec) + (current_time.tv_usec - last_print_time.tv_usec) / 1000000;
 
         // If two seconds have elapsed, print the total
-        if (elapsed_time >= 2000000) {
-            printf("Total atualizado: Quantidade = %d, Peso = %d\n", qtd_total, peso_total);
-            
+        if (elapsed_seconds >= 2)
+        {
+            printf("Total atualizado: Quantidade = %d\n", qtd_total);
+
             // Update last print time
-            last_print_time = current_time;
+            gettimeofday(&last_print_time, NULL);
         }
 
         //Listen for connections
@@ -63,8 +66,6 @@ int main()
             close(sockfd);
             return 1;
         }
-
-        printf("Servidor named pipe ouvindo em %s... \n", SOCK_PATH);
 
         //Accept connections
         memset(&remote, 0, sizeof(remote));
@@ -89,12 +90,17 @@ int main()
         // Atualiza total
         qtd_total += quantidade;
         peso_total += peso;
-    
+        items_received += quantidade;
+
+        if(items_received >= 10) {
+            printf("Total atualizado: Peso = %d\n", peso_total);
+            items_received = 0;
+        }
+
+        
     }
     
-
-    //Closes sockets and exit
-    close(newsockfd);
+    close(newsockfd);   
     close(sockfd);
     return 0;
 }
